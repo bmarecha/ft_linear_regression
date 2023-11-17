ef3464afe22058f81218caa6a396f7cde7a872c7
Marechal Baptiste <baptistemarechal42@gmail.com>
2023-11-17 17:33:11 +0100

    graph changes

--- graph.py
+++ graph.py
@@ -3,44 +3,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-if not os.path.isfile("data.csv") :
-	print("Please put data in 'data.csv'")
-	exit(1)
-file = open("data.csv", "r")
-line = file.readline().split(",")
-if (len(line) != 2) :
-	print("The data needs two columns for a proper graph")
-	exit(1)
-xpoints = []
-ypoints = []
-#Need to test if first line really is axis name or first data
-labels = line
-line = file.readline()
-while (line) :
-	coord = line.split(",")
-	xpoints.append(int(coord[0]))
-	ypoints.append(int(coord[1]))
+def readData (filename: str) -> tuple:
+	if not os.path.isfile(filename) :
+		print("Missing file :" + filename)
+		exit(1)
+	file = open(filename, "r")
+	line = file.readline().split(",")
+	if (len(line) != 2) :
+		print("The data needs two columns for a proper graph")
+		exit(1)
+	xpoints = []
+	ypoints = []
+	#Need to test if first line really is axis name or first data
+	labels = line
 	line = file.readline()
+	while (line) :
+		coord = line.split(",")
+		xpoints.append(int(coord[0]))
+		ypoints.append(int(coord[1]))
+		line = file.readline()
+	return (labels, xpoints, ypoints)
 
 
-
-th1 = 0
-th2 = 0
-if os.path.isfile(".thetas.txt") :
-	file = open(".thetas.txt", "r")
+def readThetas (filename:str) -> tuple:
+	if not os.path.isfile(filename) :
+			print("Missing file :" + filename)
+			return false
+	file = open(filename, "r")
 	line = file.readline().split()
 	file.close()
-	if (len(line) > 1) :
-		th1 = float(line[0])
-		th2 = float(line[1])
+	if (len(line) <= 1) :
+		print("Needs two floats.")
+		exit(1)
+	return (float(line[0]), float(line[1]))
+	
 
-x = np.arange(2, 250000, 4)
-y = th1 + (th2 * x)
-print(x)
-print(y)
-plt.plot(x, y)
+def main () :
+	labels, xpoints, ypoints = readData("data.csv")
+	th1, th2 = readThetas(".thetas.txt")
 
-plt.xlabel(labels[0])
-plt.ylabel(labels[1])
-plt.plot(xpoints, ypoints, 'o')
-plt.show()
+	x = np.arange(500, 250000, 4)
+	y = th1 + (th2 * x)
+	print(x)
+	print(y)
+	plt.plot(x, y)
+	plt.ylim(0, None)
+
+	plt.xlabel(labels[0])
+	plt.ylabel(labels[1])
+	plt.plot(xpoints, ypoints, 'o')
+	plt.show()
+
+if __name__ == '__main__':
+	main()
